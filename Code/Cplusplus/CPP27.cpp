94b7716af2ef1d03081f3c6b4578f4ab2fdfdd16
zzhmu <1406772069@qq.com>
2023-10-19 16:29:00 +0800

    添加项目文件。

---
+++ Code/Cplusplus/CPP27.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char str[100] = { 0 };
+    cin.getline(str, sizeof(str));
+
+    // write your code here......
+    
+
+    return 0;
+}
